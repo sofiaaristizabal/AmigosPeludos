b1afdb7e26aab8128bc9dff82c383078e2afd1d5
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-25 18:31:11 -0500

    Commented the default values in code, since we have them in files now

--- Modulos/base_de_datos.py
+++ Modulos/base_de_datos.py
@@ -9,30 +9,35 @@ general_logger = set_up_logger(__name__, file_name="run_time_logger.log")
 
 class Data_Base():
     def __init__(self):
-        self._owners: list[Owner] = [
-            Owner('Santiago Yepes', '3757353838', 'Siao Paulinho', 1),
-            Owner('Sofia Aristizabal', '3454757474', 'Ilo durado', 2),
-            Owner('Simon Gomez', '3758456363', 'Il Polles imeraldinha', 3)
-        ]
+        # self._owners: list[Owner] = [
+        #     Owner('Santiago Yepes', '3757353838', 'Siao Paulinho', 1),
+        #     Owner('Sofia Aristizabal', '3454757474', 'Ilo durado', 2),
+        #     Owner('Simon Gomez', '3758456363', 'Il Polles imeraldinha', 3)
+        # ]
 
-        self._pets: list[Pet] = [
-            Pet('Lucas', 'French Poodle', '11/02/2013', 'French Poodle', self._owners[0], 1),
-            Pet('Ilo', 'Golden Retriever', '05/07/2018', 'Golden Retriever', self._owners[1], 2),
-            Pet('Pollo', 'Bulldog', '23/09/2020', 'Bulldog', self._owners[2], 3)
-        ]
+        # self._pets: list[Pet] = [
+        #     Pet('Lucas', 'French Poodle', '11/02/2013', 'French Poodle', self._owners[0], 1),
+        #     Pet('Ilo', 'Golden Retriever', '05/07/2018', 'Golden Retriever', self._owners[1], 2),
+        #     Pet('Pollo', 'Bulldog', '23/09/2020', 'Bulldog', self._owners[2], 3)
+        # ]
 
-        self._appointments: list[Appointment] = [
-            Appointment('11/02/15', 'Vaccination', 'Teeth infection', self._pets[2], 1),
-            Appointment('12/03/16', 'General Checkup', 'Healthy', self._pets[0], 2),
-            Appointment('15/04/17', 'Deworming', 'Mild worms', self._pets[1], 3),
-            Appointment('20/05/18', 'Vaccination', 'Allergic reaction', self._pets[2], 4),
-            Appointment('22/06/19', 'Dental Cleaning', 'Tartar buildup', self._pets[0], 5),
-            Appointment('30/07/20', 'Surgery', 'Neutering', self._pets[1], 6),
-            Appointment('10/08/21', 'X-ray', 'Limping', self._pets[2], 7),
-            Appointment('14/09/22', 'Blood Test', 'Routine check', self._pets[0], 8),
-            Appointment('18/10/23', 'Vaccination', 'Healthy', self._pets[1], 9),
-            Appointment('25/11/24', 'General Checkup', 'Minor cold', self._pets[2], 10)
-        ]
+        # self._appointments: list[Appointment] = [
+        #     Appointment('11/02/15', 'Vaccination', 'Teeth infection', self._pets[2], 1),
+        #     Appointment('12/03/16', 'General Checkup', 'Healthy', self._pets[0], 2),
+        #     Appointment('15/04/17', 'Deworming', 'Mild worms', self._pets[1], 3),
+        #     Appointment('20/05/18', 'Vaccination', 'Allergic reaction', self._pets[2], 4),
+        #     Appointment('22/06/19', 'Dental Cleaning', 'Tartar buildup', self._pets[0], 5),
+        #     Appointment('30/07/20', 'Surgery', 'Neutering', self._pets[1], 6),
+        #     Appointment('10/08/21', 'X-ray', 'Limping', self._pets[2], 7),
+        #     Appointment('14/09/22', 'Blood Test', 'Routine check', self._pets[0], 8),
+        #     Appointment('18/10/23', 'Vaccination', 'Healthy', self._pets[1], 9),
+        #     Appointment('25/11/24', 'General Checkup', 'Minor cold', self._pets[2], 10)
+        # ]
+
+        self._owners: list[Owner] = []
+        self._pets: list[Pet] = []
+        self._appointments: list[Appointment] = []
+
 
     def find_owner_by_id(self, id: int):
         for owner in self._owners:
@@ -41,6 +46,7 @@ class Data_Base():
             
         raise Owner_Not_Found_Error(id)
     
+
     def find_pet_by_id(self, id: int):
         for pet in self._pets:
             if pet.id == id:
@@ -48,23 +54,30 @@ class Data_Base():
             
         raise Pet_Not_Found_Error(id)
 
+
     def get_owners(self,):
         return self._owners.copy()
+
 
     def get_pets(self,):
         return self._pets.copy()
 
+
     def get_appointments(self,):
         return self._appointments.copy()
+
 
     def add_pet(self,mascota):
         self._pets.append(mascota)
 
+
     def add_owner(self,owner):
         self._owners.append(owner)
 
+
     def add_query(self,queries):
         self._appointments.append(queries)
+
 
     def find_pet(self,nombre_mascota, nombre_owner):
         for mascota in self._pets:
@@ -73,12 +86,14 @@ class Data_Base():
 
         return None
 
+
     def find_owner(self,nombre_owner, telefono) -> Owner | None:
         for owner in self._owners:
             if owner.nombre == nombre_owner and owner.telefono == telefono:
                 return owner
 
         return None
+
 
     def query_by_pet(self,nombre_mascota, nombre_owner):
         mascota = self.find_pet(nombre_mascota, nombre_owner)
@@ -87,8 +102,10 @@ class Data_Base():
         
         return [consulta for consulta in self._appointments if consulta.mascota == mascota]
 
+
     def registered_pets(self,):
         print('\n'.join(str(mascota) for mascota in self._pets))
+
 
     def get_next_pet_id(self,):
         if len(self._pets) == 0:
@@ -112,6 +129,7 @@ class Data_Base():
                 max_id = owner.id
 
         return max_id + 1
+
 
     def get_next_appointment_id(self):
         if len(self._appointments) == 0:
