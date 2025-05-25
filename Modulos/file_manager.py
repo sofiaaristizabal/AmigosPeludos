a649b96fcc77426f2a781243ebe4a09ea25dac93
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-25 15:14:53 -0500

    Changed the File_Manager methods to be static

--- Modulos/file_manager.py
+++ Modulos/file_manager.py
@@ -14,44 +14,37 @@ logger: logging.Logger = set_up_logger(
 
 
 class File_Manager():
-    def __init__(self, path: str=".\\..\\Datos"):
-        self.path: str = path
-        self.appointment_folder: str = "appoinments"
-        self.owners_file: str = "Owners.csv"
-        self.pets_file: str = "Pets.csv"
-
-
-    def save_database(self, database: Data_Base) -> None:
+    def save_database(database: Data_Base) -> None:
         logger.info("Started to save the database")
-        self._save_pets(database.get_all_pets())
-        self._save_appointments(database.get_appointments())
-        self._save_owners(database.get_owners())
+        File_Manager._save_pets(database.get_all_pets())
+        File_Manager._save_appointments(database.get_appointments())
+        File_Manager._save_owners(database.get_owners())
         logger.info("Database saved")
 
 
-    def load_database(self, database: Data_Base) -> None:
-        self._load_pets()
-        self._load_appointments()
-        self._load_owners()
+    def load_database(database: Data_Base) -> None:
+        File_Manager._load_pets()
+        File_Manager._load_appointments()
+        File_Manager._load_owners()
 
 
-    def _load_pets(self, database: Data_Base) -> None:
+    def _load_pets(database: Data_Base) -> None:
         pass
 
 
-    def _load_appointments(self, database: Data_Base) -> None:
+    def _load_appointments(database: Data_Base) -> None:
         pass
 
 
-    def _load_owners(self, database: Data_Base) -> None:
+    def _load_owners(database: Data_Base) -> None:
         pass
     
 
-    def _save_pets(self, pets: list[Pet]) -> None:
+    def _save_pets(pets: list[Pet], path: str=".\\..\\Datos", pets_file: str="Pets.csv") -> None:
         logger.info("Saving the pets")
         # w because we want to reset the database each time
         try:
-            with open(self.path + self.pets_file, 'w') as csvfile:
+            with open(path + pets_file, 'w') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(pets[0].to_dict().keys())  # Set the column names
                 for pet in pets:  # Enter all the objets
@@ -61,23 +54,23 @@ class File_Manager():
             logger.error("The CSV file for the pets couldn't be found, pets were not saved")
 
 
-    def _save_appointments(self, appointments: list[Appointment]) -> None:
+    def _save_appointments(appointments: list[Appointment], path: str=".\\..\\Datos", appointment_folder: str="appoinments") -> None:
         logger.info("saving the appoinments")
         for appointment in appointments:
-            json_val = self._convert_to_json(appointment)
+            json_val = File_Manager._convert_to_json(appointment)
             try: 
-                with open(self.path + self.appointment_folder + f"\\apointment_{appointment.id}.json", "w") as file:
+                with open(path + appointment_folder + f"\\apointment_{appointment.id}.json", "w") as file:
                     file.write(json_val)
                 logger.debug(f"saved the appointment with id: {appointment.id}")
             except Exception as e:
                 logger.error(f"Couldnt save appointment with id: {appointment.id}, error message: {e}")
 
 
-    def _save_owners(self, owners: list[Owner]) -> None:
+    def _save_owners(owners: list[Owner], path: str=".\\..\\Datos", owners_file: str="Owners.csv") -> None:
         logger.info("Saving the owners")
         # w because we want to reset the database each time
         try:
-            with open(self.path + self.owners_file, 'w') as csvfile:
+            with open(path + owners_file, 'w') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(owners[0].to_dict().keys())  # Set the column names
                 for owner in owners:  # Enter all the objets
@@ -90,5 +83,5 @@ class File_Manager():
             logger.error(f"Couldn't finish saving all the owners, error message: {e}")
     
 
-    def _convert_to_json(self, object: dict) -> str:
+    def _convert_to_json(object: dict) -> str:
         return json.dumps(object)
