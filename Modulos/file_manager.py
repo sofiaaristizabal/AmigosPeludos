e5a0c72c52ed83fb9791b6e3dc775767b72225d8
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-25 13:52:24 -0500

    Defined thetypes the functions will receive

--- Modulos/file_manager.py
+++ Modulos/file_manager.py
@@ -1,5 +1,8 @@
 import json
 from base_de_datos import Data_Base
+from appointment import Appointment
+from owner import Owner
+from pet import Pet
 
 class File_Manager():
     def __init__(self, path=".\\..\\datos"):
@@ -9,38 +12,38 @@ class File_Manager():
         self.pet_folder = "pets"
 
 
-    def save_database(self, database: Data_Base):
+    def save_database(self, database: Data_Base) -> None:
         self._save_pets(database.get_all_pets())
         self._save_appointments(database.get_appointments())
         self._save_owners(database.get_owners())
 
 
-    def load_database(self):
+    def load_database(self, database: Data_Base) -> None:
         self._load_pets()
         self._load_appointments()
         self._load_owners()
 
 
-    def _load_pets(self):
+    def _load_pets(self, database: Data_Base) -> None:
         pass
 
 
-    def _load_appointments(self):
+    def _load_appointments(self, database: Data_Base) -> None:
         pass
 
 
-    def _load_owners(self):
+    def _load_owners(self, database: Data_Base) -> None:
         pass
     
 
-    def _save_pets(self, pets):
+    def _save_pets(self, pets: list[Pet]) -> None:
         pass
 
 
-    def _save_appointments(self, appointments):
+    def _save_appointments(self, appointments: list[Appointment]) -> None:
         pass
 
 
-    def _save_owners(self, appointments):
+    def _save_owners(self, owners: list[Owner]) -> None:
         pass
     
